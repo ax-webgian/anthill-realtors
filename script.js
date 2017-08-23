3d7e217aea8141250ec0ea8f9f3b1991108411b2
Gian Gicaraya <gp.gicaraya@gmail.com>
2017-08-23 12:25:20 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,47 @@
+$( document ).ready(function() {
+var rangeSlider = function(){
+  var slider = $('.range-slider'),
+      range = $('.range-slider__range'),
+      value = $('.range-slider__value');
+    
+  slider.each(function(){
+
+    value.each(function(){
+      var value = $(this).prev().attr('value');
+      $(this).html(value);
+    });
+
+    range.on('input', function(){
+      $(this).next(value).html(this.value);
+    });
+  });
+};
+
+rangeSlider();
+
+
+
+
+
+$( ".cross" ).hide();
+$( ".menu" ).hide();
+$( ".hamburger" ).click(function() {
+$( ".menu" ).slideToggle( "slow", function() {
+$( ".hamburger" ).hide();
+$( ".cross" ).show();
+});
+});
+
+$( ".cross" ).click(function() {
+$( ".menu" ).slideToggle( "slow", function() {
+$( ".cross" ).hide();
+$( ".hamburger" ).show();
+});
+});
+
+var reformattednumber = document.getElementById("range-closer").value;
+
+alert (reformattednumber);//
+
+});
+
